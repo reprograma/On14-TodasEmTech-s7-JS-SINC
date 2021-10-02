22f22814eaeb72b6c3750350ba730c0572ea25e9
Érika de Freitas <erikafreitas47@gmail.com>
2021-10-01 21:11:23 -0300

    insere atividade de Érika - semana 7

--- exercicios.js
+++ exercicios.js
@@ -9,34 +9,63 @@ ferramentas aprendidas nesta aula para resolver o código assíncrono e obter o 
 */
 
 function buscarPreco(produto) {
-  setTimeout(() => {
-    if (produto === "notebook") {
-      return {
-        nome: "notebook",
-        preco: 3499.0,
-      };
-    } else if (produto === "smartphone") {
-      return {
-        nome: "smartphone",
-        preco: 1999.9,
-      };
-    } else if (produto === "tablet") {
-      return {
-        nome: "tablet",
-        preco: 2999.9,
-      };
-    } else {
-      return "Produto não encontrado";
-    }
-  }, 2000);
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+
+      if (produto === "notebook") {
+        return resolve  ({
+          nome: "notebook",
+          preco: 3499.0,
+        });  
+      } 
+      
+      else if (produto === "smartphone") {
+          return resolve ({
+          nome: "smartphone",
+          preco: 1999.9,
+        });
+      }
+      
+      else if (produto === "tablet") {
+        return resolve ({
+          nome: "tablet",
+          preco: 2999.9,
+        });
+      } 
+
+        else {
+        return reject ("Produto não encontrado");
+      }
+    }, 2000);    
+  })
 }
 
-function calcularParcela(preco) {
-  let parcelasDesejadas = 10;
-  setTimeout(() => {
-    return preco * parcelasDesejadas;
-  }, 2000);
+function calcularParcela(preco, parcelasDesejadas) {
+  //let parcelasDesejadas = 10;
+  return new Promise ((resolve) => {
+      setTimeout(() => {
+        return resolve(preco / parcelasDesejadas);
+      }, 2000);
+  })    
 }
+
+resolver = async (produto, parcelasDesejadas) => {
+  try {
+    const dadosdoProduto = await buscarPreco(produto);    
+    const valorDaParcela = await calcularParcela(dadosdoProduto.preco, parcelasDesejadas);
+
+    let valorTotal = (dadosdoProduto.preco).toFixed(2);
+    let vaorFinalDaParcela = valorDaParcela.toFixed(2);
+
+    console.log(`Seu notebook custa R$${valorTotal.replace(".",",")} e você pagará em ${parcelasDesejadas}x de ${vaorFinalDaParcela.replace(".",",")}`);  
+  }
+  catch(err) {
+    console.error("Retornou um erro: " + err);
+  }    
+}
+
+resolver("notebook", 10);
 
 /*
 2. Resolva usando async/await:
@@ -69,10 +98,10 @@ function buscarPrecoDolar() {
 
 function buscarJurosImportacao() {
   return new Promise((resolve) => {
-    setTimeout(() => {
+    setTimeout(() => {      
       resolve({
-        juros1: 0.05,
-        juros2: 0.09,
+        juros1: 0.05, //5%
+        juros2: 0.09, //9%
         messages:
           "os dois juros são aplicados no valor total do produto em real",
       });
@@ -82,5 +111,20 @@ function buscarJurosImportacao() {
 
 async function calcularValorEmReal(precoEmDolar) {
   try {
-  } catch (error) {}
+    const dolar = await buscarPrecoDolar();
+    //console.log("Dólar comercial: " + dolar.comercial);
+    const juros = await buscarJurosImportacao();
+    //console.log(juros.juros1);
+    let precoEmReal = precoEmDolar *  dolar.comercial;
+    //console.log(precoEmReal);
+    let precoFinal = precoEmReal + (precoEmReal * juros.juros1) + (precoEmReal  * juros.juros2);
+    console.log(`O preço final do seu produto é R$ ${precoFinal.toFixed(2).replace(".",",")}`);
+  } 
+    catch (error) {
+      console.error("Erro capturado: " + error);
+    }
 }
+
+calcularValorEmReal(850);
+
+
--- index.js
+++ index.js
@@ -11,3 +11,18 @@
     2. A partir da id da usuária obter o endereço cadastrado;
     3. A partir da id da usuária obter o histórico de pedidos;
 */
+
+function pegarUsuaria () {
+  setTimeout(() => {
+    return {
+      nome : "Pepita",
+      email: "mulherpepita@gmail.com"
+    }
+    
+  }, 2000);
+}
+
+const usuaria = pegarUsuaria();
+
+console.log("Usuária: " + usuaria );
+
