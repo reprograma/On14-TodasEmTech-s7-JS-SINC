230aba6d47325f88c1d6af0bfa2271f717202d25
Estefanya <estefanya.nascimento@outlook.com>
2021-10-06 08:52:49 -0300

    Atividade 7

--- exercicios.js
+++ exercicios.js
@@ -9,34 +9,68 @@ ferramentas aprendidas nesta aula para resolver o código assíncrono e obter o 
 */
 
 function buscarPreco(produto) {
+  return new Promise((resolve, reject) => {
   setTimeout(() => {
     if (produto === "notebook") {
-      return {
+      return resolve({
         nome: "notebook",
         preco: 3499.0,
-      };
+      });
     } else if (produto === "smartphone") {
-      return {
+      return resolve({
         nome: "smartphone",
         preco: 1999.9,
-      };
+      });
     } else if (produto === "tablet") {
-      return {
+      return resolve({
         nome: "tablet",
         preco: 2999.9,
-      };
+      });
     } else {
-      return "Produto não encontrado";
+      return reject("Produto não encontrado");
     }
+
   }, 2000);
+})
+
 }
 
-function calcularParcela(preco) {
-  let parcelasDesejadas = 10;
+function calcularParcela(preco, parcelasDesejadas) {
+  //let parcelasDesejadas = 10;
+  return new Promise ((resolve) => {
   setTimeout(() => {
-    return preco / parcelasDesejadas;
+    return resolve (preco / parcelasDesejadas);
   }, 2000);
+});
 }
+
+
+
+resolver = async(produto, parcelasDesejadas) => {//ao usar async dizemos que esta função é assincrona, ou seja, retorna uma promessa
+
+  try{//try significa que o javascript precisa executar tudo que estiver dentro deste { escopo } e se der algum erro, ele deve ser capturado pelo catch
+    
+    const preco = await buscarPreco(produto);//o await significa que o javascript precisa aguardar a resolução da função assincrona passada pra poder armazenar o retorno antes de prosseguir
+    const valorDaParcela = await calcularParcela(preco.preco, parcelasDesejadas);
+  
+    let valorTotal = (preco.preco).toFixed(2);
+    let valorFinalDaParcela = valorDaParcela.toFixed(2);
+
+    console.log(`Seu notebook custa R$${valorTotal.replace('.',',')} e você pagará em 10x de R$${valorFinalDaParcela}`)
+
+  
+  } 
+  catch (err) {
+    //captura qualquer erro que ocorrer dentro do try sem permitir que o código quebre
+    console.error("Capturamos um erro: ", err); 
+  }
+}
+
+resolver('notebook', 10) //precisamos chamar a função 
+
+
+
+
 
 /*
 2. Resolva usando async/await:
@@ -56,7 +90,7 @@ dica: valor em real + (valor em real * juros1) + (valor em real * juros2) = valo
 
 */
 
-function buscarPrecoDolar() {
+ function buscarPrecoDolar() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
@@ -80,7 +114,24 @@ function buscarJurosImportacao() {
   });
 }
 
+const precoEmDolar = 850  //preço em dólar
+
 async function calcularValorEmReal(precoEmDolar) {
   try {
-  } catch (error) {}
-}
+    const valorDolar = await buscarPrecoDolar();//o await significa que o javascript precisa aguardar a resolução da função assincrona passada pra poder armazenar o retorno antes de prosseguir
+
+    const buscarJuros  = await buscarJurosImportacao(); //o await só pode ser usado em funções async
+   
+    let valorEmReal = precoEmDolar * valorDolar.comercial;
+    let valorTotal = valorEmReal + buscarJuros.juros1 * valorEmReal + buscarJuros.juros2 * valorEmReal
+
+    console.log(`O preço final do seu produto é R$${valorTotal.toFixed(2).replace('.', ',')}`);
+
+
+    } catch (err) {
+      //captura qualquer erro que ocorrer dentro do try sem permitir que o código quebre
+      console.error("Capturamos um erro: ", err);
+    }
+    }
+  
+    calcularValorEmReal(850)
