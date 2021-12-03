3dacde59232a3d7230aabd7988dac108eb3764a7
Ana Carolliny <80598323+anacarolliny@users.noreply.github.com>
2021-12-03 16:14:18 -0300

    enviando exercicios

---
+++ exercicio1.js
@@ -0,0 +1,62 @@
+/*
+1. Precisamos calcular o valor da parcela de um notebook que a Bigbig vai comprar, para isso precisamos descobrir o 
+preço do produto e em seguida o número de parcelas desejadas, a partir das funções a seguir, utilize qualquer uma das
+ferramentas aprendidas nesta aula para resolver o código assíncrono e obter o seguinte retorno no console: 
+"Seu notebook custa R$3499,00 e você pagará em 10x de R$349,90"
+*/
+
+function buscarPreco(produto) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (produto === "notebook") {
+          return resolve({
+            nome: "notebook",
+            preco: 3499.0,
+          });
+        } else if (produto === "smartphone") {
+          return resolve({
+            nome: "smartphone",
+            preco: 1999.9,
+          });
+        } else if (produto === "tablet") {
+          return resolve({
+            nome: "tablet",
+            preco: 2999.9,
+          });
+        } else {
+          return reject("Produto não encontrado");
+        }
+      }, 2000);
+    });
+  }
+  
+  function calcularParcela(preco) {
+    return new Promise((resolve) => {
+      let parcelasDesejadas = 10;
+      setTimeout(() => {
+        return resolve(preco / parcelasDesejadas);
+      }, 2000);
+    });
+  }
+  
+  notebookBigbig = async (produto, parcelasDesejadas) => {
+    try {
+      const capturarProdutos = await buscarPreco(produto);
+      const calcularAParcela = await calcularParcela(
+        capturarProdutos.preco,
+        parcelasDesejadas
+      );
+      let valorFinal = capturarProdutos.preco.toFixed(2);
+      let parcelaFinal = calcularAParcela.toFixed(2);
+      console.log(
+        `Seu notebook custa R$${valorFinal.replace(
+          ".",
+          ","
+        )} e você pagará em ${parcelasDesejadas}x de R$${parcelaFinal}`
+      );
+    } catch (err) {
+      console.error("Encontramos um erro: ", err);
+    }
+  };
+  
+  notebookBigbig("notebook", 10);
--- exercicios.js
+++ exercicios.js
@@ -1,11 +1,8 @@
 /*
-
 1. Precisamos calcular o valor da parcela de um notebook que a Bigbig vai comprar, para isso precisamos descobrir o 
 preço do produto e em seguida o número de parcelas desejadas, a partir das funções a seguir, utilize qualquer uma das
 ferramentas aprendidas nesta aula para resolver o código assíncrono e obter o seguinte retorno no console: 
-
 "Seu notebook custa R$3499,00 e você pagará em 10x de R$349,90"
-
 */
 
 function buscarPreco(produto) {
@@ -34,26 +31,20 @@ function buscarPreco(produto) {
 function calcularParcela(preco) {
   let parcelasDesejadas = 10;
   setTimeout(() => {
-    return preco * parcelasDesejadas;
+    return preco / parcelasDesejadas;
   }, 2000);
 }
 
 /*
 2. Resolva usando async/await:
-
 Você quer saber quanto vai pagar em reais por um produto comprado nos EUA e para isso precisa consultar numa "API" 
 de contação para descobrir o valor do Dólar no momento da compra (você deve usar o valor do dólar comercial) e calcular 
 o valor em Real, em seguida precisa consultar outra "API" que retorna o valor de dois juros que serão cobrados sob o 
 preço em Real e retornar o valor final
-
 dados:
-
 const precoEmDolar = 850  //preço em dólar
-
 valor de retorno no console: "O preço final do seu produto é R$5096,94"
-
 dica: valor em real + (valor em real * juros1) + (valor em real * juros2) = valor final
-
 */
 
 function buscarPrecoDolar() {
